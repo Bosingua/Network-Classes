bf3915caef64313bd8b960de7fdd85b467ff3ae5
Bosingua <robertobosi@gmail.com>
2024-10-16 19:14:00 +0200

    Create MACAddress.py

---
+++ MACAddress.py
@@ -0,0 +1,42 @@
+import re
+
+class MACAddress:
+
+    def __init__(self, macAddress : str):
+        if not self.is_valid(macAddress):
+            raise ValueError('Invalid MAC address')
+        
+        if MACAddress._formato == 'Colon-separated':
+            self._macAddress = macAddress.replace(":", "").lower()
+        elif MACAddress._formato == 'Hyphen-separated':
+            self._macAddress = macAddress.replace("-", "").lower()
+        elif MACAddress._formato == 'Dot-separated':
+            self._macAddress = macAddress.replace(".", "").lower()
+        elif MACAddress._formato == 'Hexadecimal string':
+            self._macAddress = macAddress.lower()
+
+    @staticmethod
+    def is_valid(mac_address : str) -> bool:
+        if re.fullmatch(re.compile(r'([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})'), mac_address):
+            MACAddress._formato = 'Colon-separated'
+            return True
+        elif re.fullmatch(re.compile(r'([0-9A-Fa-f]{2}[-]){5}([0-9A-Fa-f]{2})'), mac_address):
+            MACAddress._formato = 'Hyphen-separated'
+            return True
+        elif re.fullmatch(re.compile(r'([0-9A-Fa-f]{4}[.]){2}([0-9A-Fa-f]{4})'), mac_address):
+            MACAddress._formato = 'Dot-separated'
+            return True
+        elif re.fullmatch(re.compile(r'([0-9A-Fa-f]{12})'), mac_address):
+            MACAddress._formato = 'Hexadecimal string'
+            return True
+        else:
+            return False
+
+    def in_Colon_separated_format(self) -> str:
+        return ':'.join([self._macAddress[i:i+2].upper() for i in range(0, 12, 2)])
+
+    def in_Hyphen_separated_format(self) -> str:
+        return '-'.join([self._macAddress[i:i+2].upper() for i in range(0, 12, 2)])
+
+    def in_dot_separated_format(self) -> str:
+        return '.'.join([self._macAddress[i:i+4] for i in range(0, 12, 4)])

0fd88f2c579d947eecd50d952dd19d2b4f785974
Bosingua <robertobosi@gmail.com>
2024-11-15 00:53:47 +0100

    Aggiunto __ne__ ed altri Magic Methods

--- MACAddress.py
+++ MACAddress.py
@@ -6,43 +6,47 @@ class MACAddress:
     def _is_valid(self, input) -> bool:
         if not isinstance(input, str): return False
         input = input.lower()
-        pattern = r'[\da-f]{12}|[\da-f]{2}([:-][\da-f]{2}){5}|[\da-f]{4}\.[\da-f]{4}\.[\da-f]{4}'
+        pattern = r'[\da-f]{12}|[\da-f]{2}([:-][\da-f]{2}){5}|([\da-f]{4}\.){2}[\da-f]{4}'
         if not fullmatch(pattern, input): return False
         return fullmatch(r'[\da-f]{12}', input.translate(str.maketrans('', '', ':-.')))
 
-    def _mac_in_memory(self, mac : str) -> str: return mac.translate(str.maketrans('', '', ':-.')).lower()
+    def __init__(self, input: str) -> None: self._mac_address = input
 
-    def __init__(self, input : str) -> None: self._mac_address = input
-
-    def __setattr__(self, name : str, value):
+    def __setattr__(self, name: str, value) -> None:
         if hasattr(self, '_mac_address'): raise ValueError(f"cannot assign to field '{name}'")
         if not self._is_valid(value): raise ValueError('Invalid MAC address')
-        super().__setattr__(name, self._mac_in_memory(value))
+        if isinstance(value, str): super().__setattr__(name, value.translate(str.maketrans('','',':-.')).lower())
 
-    def _format_mac_address(self, separator : str, upcase : bool, len : int = 2) -> str:
+    def _format_mac_address(self, separator: str, upcase: bool, len: int = 2) -> str:
         mac = self._mac_address.upper() if upcase else self._mac_address
         return separator.join([mac[i:i+len] for i in range(0, 12, len)])
 
-    def in_Colon_separated_format(self, upcase : bool = True) -> str: return self._format_mac_address(":", upcase)
+    def in_Colon_separated_format(self, upcase: bool = True) -> str: return self._format_mac_address(":", upcase)
 
-    def in_Hyphen_separated_format(self, upcase : bool = True) -> str: return self._format_mac_address("-", upcase)
+    def in_Hyphen_separated_format(self, upcase: bool = True) -> str: return self._format_mac_address("-", upcase)
 
-    def in_dot_separated_format(self, upcase : bool = False) -> str: return self._format_mac_address(".", upcase, 4)
+    def in_dot_separated_format(self, upcase: bool = False) -> str: return self._format_mac_address(".", upcase, 4)
 
     def __add__(self): raise ValueError
 
-    __radd__ = __sub__ =__truediv__ = __div__ = __mod__ = __pow__ = __add__
+    __radd__ = __sub__ =__truediv__ = __div__ = __mod__ = __pow__ = __floordiv__ = __add__
+    __rsub__ =__rtruediv__ = __rdiv__ = __rmod__ = __rpow__ = __rfloordiv__ = __add__
+    __lt__ = __le__ = __ge__ = __gt__ = __add__
 
     def __eq__(self, other) -> bool:
         if isinstance(other, MACAddress): return self._mac_address == other._mac_address
-        return self._mac_address == self._mac_in_memory(other) if self._is_valid(other) else False
+        if isinstance(other, str): return self._mac_address == other.translate(str.maketrans('','',':-.')).lower()
+        return False
+
+    def __ne__(self, other) -> bool: return not self.__eq__(other)
 
     def __hash__(self) -> str: return self._mac_address
 
     def __bool__(self) -> bool: return True
 
     @staticmethod
-    def random_generator(input = ''):
+    def random_generator(input: str = ''):
+        if not isinstance(input, str): raise TypeError
         input = input.translate(str.maketrans('', '', ':-.')).lower()
         if len(input) > 13 and not fullmatch(r'[\da-f]?', input): raise ValueError
-        return MACAddress(f'{input}{''.join([format(randint(0,15), 'x') for _ in range(12 - len(input))])}')
+        return MACAddress(f'{input}{''.join([format(randint(0, 15), 'x') for _ in range(12 - len(input))])}')
--- test_MAC.py
+++ test_MAC.py
@@ -10,29 +10,46 @@ class testMAC(unittest.TestCase):
         self.assertEqual(     MACAddress('002596FFfe1a')._mac_address, '002596fffe1a')
         test_input = [' 00:25:96:FF:fe:1a', '96:FF:fe:1a:34:56 ', '00:2u:96:FF:FE:12', '00:25:96e:FF:FE:12',
         '00:25:96:F:FE:12', '00:25:96:FF:FE:112', '00', '00:25', '00:25:96', '00:25:96:FF', '00:25:96:FF:fe',
-        2, [6,'R'], ([51,'R'], 4, 'dede'),'002:5:96:FF:fe:1a']
+        2, [6,'R'], ([51,'R'], 4, 'dede'), '002:5:96:FF:fe:1a', '002.596FF.fe1a']
         for input in test_input:
-            with self.assertRaises(ValueError) as error: MACAddress(input)
+            with self.assertRaises(ValueError) as error:MACAddress(input)
             self.assertEqual(str(error.exception), 'Invalid MAC address')
 
     def test_in_Colon_separated_format(self):
-        self.assertEqual(MACAddress('18-C0-4D-87-74-8F').in_Colon_separated_format(),      '18:C0:4D:87:74:8F')
+        self.assertEqual(MACAddress('18-C0-4D-87-74-8F').in_Colon_separated_format()     , '18:C0:4D:87:74:8F')
         self.assertEqual(MACAddress('18-C0-4D-87-74-8F').in_Colon_separated_format(False), '18:c0:4d:87:74:8f')
 
     def test_in_Hyphen_separated_format(self):
-        self.assertEqual(MACAddress('18:C0:4D:87:74:8F').in_Hyphen_separated_format(),      '18-C0-4D-87-74-8F')
+        self.assertEqual(MACAddress('18:C0:4D:87:74:8F').in_Hyphen_separated_format()     , '18-C0-4D-87-74-8F')
         self.assertEqual(MACAddress('18:C0:4D:87:74:8F').in_Hyphen_separated_format(False), '18-c0-4d-87-74-8f')
 
     def test_in_dot_separated_format(self):
         self.assertEqual(MACAddress('18:C0:4D:87:74:8F').in_dot_separated_format(True), '18C0.4D87.748F')
-        self.assertEqual(MACAddress('18:C0:4D:87:74:8F').in_dot_separated_format(),     '18c0.4d87.748f')
+        self.assertEqual(MACAddress('18:C0:4D:87:74:8F').in_dot_separated_format()    , '18c0.4d87.748f')
 
     def test___eq__(self):
         self.assertEqual(MACAddress('18-C0-4D-87-74-8F') == MACAddress('18:C0:4D:87:74:8F'), True)
-        self.assertEqual(MACAddress('18-C0-4D-87-74-8F') == '18:C0:4D:87:74:8F',             True)
-        self.assertEqual('18-C0-4D-87-74-8Fd'            == MACAddress('18:C0:4D:87:74:8F'), False)
-        self.assertEqual('0025.96FF.fe1a'                == MACAddress('18:C0:4D:87:74:8F'), False)
-        self.assertEqual(1111111111111111                == MACAddress('18:C0:4D:87:74:8F'), False)
+        self.assertEqual(MACAddress('18-C0-4D-87-74-8F') == '18:C0:4D:87:74:8F'            , True)
+        self.assertEqual(            '18-C0-4D-87-74-8F' == MACAddress('18:C0:4D:87:74:8F'), True)
+        self.assertEqual(               '0025.96FF.fe1a' == MACAddress('18:C0:4D:87:74:8F'), False)
+        test_input = [' 00:25:96:FF:fe:1a', '96:FF:fe:1a:34:56 ', '00:2u:96:FF:FE:12', '00:25:96e:FF:FE:12',
+        '00:25:96:F:FE:12', '00:25:96:FF:FE:112', '00', '00:25', '00:25:96', '00:25:96:FF', '00:25:96:FF:fe',
+        2, [6,'R'], ([51,'R'], 4, 'dede'),'002:5:96:FF:fe:1a']
+        for input in test_input:
+            self.assertEqual(input == MACAddress('18:C0:4D:87:74:8F'), False, f'input = {input}')
+            self.assertEqual(MACAddress('18:C0:4D:87:74:8F') == input, False, f'input = {input}')
+
+    def test___ne__(self):
+        self.assertEqual(MACAddress('18-C0-4D-87-74-8F') != MACAddress('18:C0:4D:87:74:8F'), False)
+        self.assertEqual(MACAddress('18-C0-4D-87-74-8F') != '18:C0:4D:87:74:8F'            , False)
+        self.assertEqual(            '18-C0-4D-87-74-8F' != MACAddress('18:C0:4D:87:74:8F'), False)
+        self.assertEqual(               '0025.96FF.fe1a' != MACAddress('18:C0:4D:87:74:8F'), True)
+
+    def test_random_generator(self):
+        random_mac = MACAddress.random_generator()
+        self.assertEqual(random_mac._mac_address, MACAddress.random_generator(random_mac._mac_address)._mac_address)
+        with self.assertRaises(TypeError) as error: MACAddress.random_generator([1,5])
+        with self.assertRaises(ValueError) as error: MACAddress.random_generator('ged1695')
 
     def test_immutable(self):
         mac = MACAddress('18-C0-4D-87-74-8F')
